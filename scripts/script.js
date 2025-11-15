ba98ec481de54fb10f63288814cbb8438d4e005b
Glamnart <favournchess192@gmail.com>
2025-11-15 21:18:51 +0100

    Adds back change of questions by tapping numbers  now with seperate subject questions and options

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,214 @@
+let questions
+let answersA
+let answersB
+let answersC
+let answersD
 
 if ( localStorage.subjects == "English")
 {
     document.getElementById("subject").innerHTML = "English Studies";
+    questions = [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam officia iste vel, nemo recusandae ab quia quisconsequuntur animi corrupti exercitationem quod veniam inlaboriosam sunt praesentium at molestiae mollitia vero ipsa? Vel soluta aspernatur repellendus saepe officia libero. Natu eaque dicta deserunt aperiam molestiae adipisci consecteturipsum. Illum ducimus ipsa officia accusantium! Vitae, cumquecorporis. At enim consequatur odit.",
+        "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu",
+        "okpomi  ggyufu  fytyftyf7y ytf7iyfiufy i uyfuyf7 yuf uyguyuguhg ufuyf uy y uyguygugug uguy uygu ugu guygug uguyh",
+        "Jhg uytvyc gf gftgcytchc  utfufyuguuiyg yfuyfuyf uyfug uff uyfiuyguguygyuu uguygyugyuggyugyg uy yu gygyugyuuy g ",
+        "jhjhvvg vuuttuvu uvyuuggvu yuyuuuyv f yffytfyt ytfyf ytfytfyfyy fytdesdrtdd dtrdudyudytd rts  dyrtd uydtrtdytdyt",
+        "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu",
+        "okpomi  ggyufu  fytyftyf7y ytf7iyfiufy i uyfuyf7 yuf uyguyuguhg ufuyf uy y uyguygugug uguy uygu ugu guygug uguyh",
+        "Jhg uytvyc gf gftgcytchc  utfufyuguuiyg yfuyfuyf uyfug uff uyfiuyguguygyuu uguygyugyuggyugyg uy yu gygyugyuuy g ",
+        "jhjhvvg vuuttuvu uvyuuggvu yuyuuuyv f yffytfyt ytfyf ytfytfyfyy fytdesdrtdd dtrdudyudytd rts  dyrtd uydtrtdytdyt",
+        "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu"
+    ]
+
+    answersA = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "4783686934",
+        "dSTYAGG78",
+        "usdggfigs",
+        "wveyvytarv",
+        "2656345",
+        "4783686934",
+        "dSTYAGG78",
+        "usdggfigs",
+        "wveyvytarv"
+    ]
+
+    answersB = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "riisoiwr",
+        "Fiusdfiuf",
+        "5894859",
+        "46567345763",
+        "87efeuio",
+        "riisoiwr",
+        "Fiusdfiuf",
+        "5894859",
+        "46567345763"
+    ]
+
+    answersC= [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "fhiausdi",
+        "3736264",
+        "4364644",
+        "fhsjgt67e",
+        "dgiuai",
+        "fhiausdi",
+        "3736264",
+        "4364644",
+        "fhsjgt67e"
+    ]
+
+    answersD = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "EWUY78363",
+        "8385824",
+        "1E3W1333",
+        "34443DZZ",
+        "2366BBVB",
+        "EWUY78363",
+        "8385824",
+        "1E3W1333",
+        "34443DZZ"
+    ]
+
 }   
 else if ( localStorage.subjects == "Basic Science")
 {
     document.getElementById("subject").innerHTML = "Basic Science";
+    questions = [
+    "Lorem scientist scienscular or sit amet consectetur adipisicing elit.Quisquam officia iste vel, nemo recusandae ab quia quisconsequuntur animi corrupti exercitationem quod veniam inlaboriosam sunt praesentium at molestiae mollitia vero ipsa? Vel soluta aspernatur repellendus saepe officia libero. Natu eaque dicta deserunt aperiam molestiae adipisci consecteturipsum. Illum ducimus ipsa officia accusantium! Vitae, cumquecorporis. At enim consequatur odit.",
+    "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu",
+    "okpomi  ggyufu  fytyftyf7y ytf7iyfiufy i uyfuyf7 yuf uyguyuguhg ufuyf uy y uyguygugug uguy uygu ugu guygug uguyh",
+    "Jhg uytvyc gf gftgcytchc  utfufyuguuiyg yfuyfuyf uyfug uff uyfiuyguguygyuu uguygyugyuggyugyg uy yu gygyugyuuy g ",
+    "jhjhvvg vuuttuvu uvyuuggvu yuyuuuyv f yffytfyt ytfyf ytfytfyfyy fytdesdrtdd dtrdudyudytd rts  dyrtd uydtrtdytdyt",
+    "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu",
+    "okpomi  ggyufu  fytyftyf7y ytf7iyfiufy i uyfuyf7 yuf uyguyuguhg ufuyf uy y uyguygugug uguy uygu ugu guygug uguyh",
+    "Jhg uytvyc gf gftgcytchc  utfufyuguuiyg yfuyfuyf uyfug uff uyfiuyguguygyuu uguygyugyuggyugyg uy yu gygyugyuuy g ",
+    "jhjhvvg vuuttuvu uvyuuggvu yuyuuuyv f yffytfyt ytfyf ytfytfyfyy fytdesdrtdd dtrdudyudytd rts  dyrtd uydtrtdytdyt",
+    "ft ydydyt einstein issac zuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu"
+    ]
+
+    answersA = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "4783686934",
+        "dSTYAGG78",
+        "usdggfigs",
+        "wveyvytarv",
+        "2656345",
+        "4783686934",
+        "dSTYAGG78",
+        "usdggfigs",
+        "wveyvytarv"
+    ]
+
+    answersB = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "riisoiwr",
+        "Fiusdfiuf",
+        "5894859",
+        "46567345763",
+        "87efeuio",
+        "riisoiwr",
+        "Fiusdfiuf",
+        "5894859",
+        "46567345763"
+    ]
+
+    answersC= [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "fhiausdi",
+        "3736264",
+        "4364644",
+        "fhsjgt67e",
+        "dgiuai",
+        "fhiausdi",
+        "3736264",
+        "4364644",
+        "fhsjgt67e"
+    ]
+
+    answersD = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "EWUY78363",
+        "8385824",
+        "1E3W1333",
+        "34443DZZ",
+        "2366BBVB",
+        "EWUY78363",
+        "8385824",
+        "1E3W1333",
+        "34443DZZ"
+    ]
+
+}
+else 
+{
+    questions = [
+    "Lorem ipsum dolor sit mathematics mathsis adipisicing elit.Quisquam officia iste vel, nemo recusandae ab quia quisconsequuntur animi corrupti exercitationem quod veniam inlaboriosam sunt praesentium at molestiae mollitia vero ipsa? Vel soluta aspernatur repellendus saepe officia libero. Natu eaque dicta deserunt aperiam molestiae adipisci consecteturipsum. Illum ducimus ipsa officia accusantium! Vitae, cumquecorporis. At enim consequatur odit.",
+    "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu",
+    "okpomi  ggyufu  fytyftyf7y ytf7iyfiufy i uyfuyf7 yuf uyguyuguhg ufuyf uy y uyguygugug uguy uygu ugu guygug uguyh",
+    "Jhg uytvyc gf gftgcytchc  utfufyuguuiyg yfuyfuyf uyfug uff uyfiuyguguygyuu uguygyugyuggyugyg uy yu gygyugyuuy g ",
+    "jhjhvvg vuuttuvu uvyuuggvu yuyuuuyv f yffytfyt ytfyf ytfytfyfyy fytdesdrtdd dtrdudyudytd rts  dyrtd uydtrtdytdyt",
+    "ft ydydyt dytdzuf ukuyg ug8yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu",
+    "okpomi  ggyufu  fytyftyf7y ytf7iyfiufy i uyfuyf7 yuf uyguyuguhg ufuyf uy y uyguygugug uguy uygu ugu guygug uguyh",
+    "Jhg uytvyc gf gftgcytchc  utfufyuguuiyg yfuyfuyf uyfug uff uyfiuyguguygyuu uguygyugyuggyugyg uy yu gygyugyuuy g ",
+    "jhjhvvg vuuttuvu uvyuuggvu yuyuuuyv f yffytfyt ytfyf ytfytfyfyy fytdesdrtdd dtrdudyudytd rts  dyrtd uydtrtdytdyt",
+    "ft ydydyt dytdzuf ugraphical  functional yufudyt  yigigiyf ugiugi gigyt giukiug78tgiyjgfuy  uygugugu  uygugugiugig yguigiu ugu"
+    ]
+
+    answersA = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "4783686934",
+        "dSTYAGG78",
+        "usdggfigs",
+        "wveyvytarv",
+        "2656345",
+        "4783686934",
+        "dSTYAGG78",
+        "usdggfigs",
+        "wveyvytarv"
+    ]
+
+    answersB = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "riisoiwr",
+        "Fiusdfiuf",
+        "5894859",
+        "46567345763",
+        "87efeuio",
+        "riisoiwr",
+        "Fiusdfiuf",
+        "5894859",
+        "46567345763"
+    ]
+
+    answersC= [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "fhiausdi",
+        "3736264",
+        "4364644",
+        "fhsjgt67e",
+        "dgiuai",
+        "fhiausdi",
+        "3736264",
+        "4364644",
+        "fhsjgt67e"
+    ]
+
+    answersD = [
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Necessitatibus, commodi? Eaque tempore atque ea odit!",
+        "EWUY78363",
+        "8385824",
+        "1E3W1333",
+        "34443DZZ",
+        "2366BBVB",
+        "EWUY78363",
+        "8385824",
+        "1E3W1333",
+        "34443DZZ"
+    ]
+
 }
 
 let qOne = document.getElementsByClassName("option-number")[4];
