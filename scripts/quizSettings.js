936950e5db76cb28f0989570567100e1572fb506
Glamnart <favournchess192@gmail.com>
2025-12-04 12:56:18 +0100

    Keeps name when choosing to restart quiz

--- scripts/quizSettings.js
+++ scripts/quizSettings.js
@@ -1,5 +1,9 @@
 // Ensure proper management of local storage
-document.addEventListener("DOMContentLoaded", localStorage.clear());
+document.addEventListener("DOMContentLoaded", function (){
+    if (localStorage.name)
+        document.getElementById("name").value = localStorage.name;
+    localStorage.clear()
+});
 
 
 // Gets information about subject, stores in local storage for use in other files and moves to the next webpage.
@@ -7,8 +11,10 @@ document.addEventListener("submit", function (e){
     e.preventDefault();
     const formData = new FormData(document.getElementById("quizSettings"));
     const subjectName = formData.get("subjects");
-    
+    const userName = formData.get("name");
+
     localStorage.setItem("subjects", subjectName);
+    localStorage.setItem("name", userName);
 
     window.location.href= "quizSpace.html";
 });
